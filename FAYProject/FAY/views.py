702646767f92ec2b4340143dd061fa9a50dabcd3
Lucky31001 <kilian.dumaire@gmail.com>
2026-01-12 14:05:33 +0100

    change terminologie

--- FAYProject/FAY/views.py
+++ FAYProject/FAY/views.py
@@ -28,7 +28,7 @@ def register_view(request):
     refresh = RefreshToken.for_user(user)
     return Response({
         'refresh_token': str(refresh),
-        'jwt_token': str(refresh.access_token),
+        'access_token': str(refresh.access_token),
     }, status=status.HTTP_201_CREATED)
 
 @api_view(['POST'])
@@ -40,8 +40,8 @@ def login_view(request):
     if user:
         refresh = RefreshToken.for_user(user)
         return Response({
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
+            'refresh_token': str(refresh),
+            'access_token': str(refresh.access_token),
         })
     return Response({'error': 'Identifiants invalides'}, status=status.HTTP_401_UNAUTHORIZED)
 
