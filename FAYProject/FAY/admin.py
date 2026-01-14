ca0adc60e3f82aefe687cd8254a16b971a189220
Lucky31001 <kilian.dumaire@gmail.com>
2026-01-14 11:57:37 +0100

    update admin panel

--- FAYProject/FAY/admin.py
+++ FAYProject/FAY/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from FAY.models.model_event import Event
 from FAY.models.model_profile import Profile
 
 
@@ -6,3 +8,9 @@ from FAY.models.model_profile import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'role')
     search_fields = ('user__username',)
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'location', 'price', 'event_type', 'note', 'capacity', 'creator')
+    search_fields = ('name', 'location', 'event_type', 'creator__username')
+    list_filter = ('event_type', 'date', 'creator')
